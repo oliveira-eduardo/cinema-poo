a2d6ae9fa014dd7f7d684d8cd12b8160754769a1
JoaoBelo23 <joao.belo.cs@gmail.com>
2026-03-06 15:47:39 -0300

    Mudanca na class bilhete

--- Bilhete.java
+++ Bilhete.java
@@ -1,21 +1,7 @@
 public class Bilhete {
-    private String user;
-    private String cpf;
-    private String sala;
-    private String sessao;
-    private String nome_do_filme;
-    private double valor;
-    private String cadeira_comprada;
-    
-    public Bilhete(String user, String cpf, String sala, String sessao, String nome_do_filme, Double valor, String cadeira_comprada) {
-
-        this.user = user;
-        this.cpf = cpf;
-        this.sala = sala;
-        this.sessao = sessao;
-        this.nome_do_filme = nome_do_filme;
-        this.valor = valor;
-        this.cadeira_comprada = cadeira_comprada;
-
-    }
+    private Usuario usuario; // Nome e Cpf 
+    private Filme filme; //Nome do filme
+    private Sessao sessao;
+    private Sala sala; 
+    private Compra compra; // Valor e cadeira comprada
 }
