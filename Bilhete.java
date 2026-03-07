3150332105e25591d3ed32d477ace798b7231032
Eduardo Oliveira <oliveira.eduardo1104@gmail.com>
2026-03-07 17:32:44 -0300

    passando a funcao de cadastro para a classe usuario

--- Bilhete.java
+++ Bilhete.java
@@ -1,7 +1,8 @@
 public class Bilhete {
     private String nome; 
     private String cpf;
-    private String filme; 
+    private String filme;
+    private String horario; 
     private String sessao;
     private String sala; 
     private float  valor;
@@ -10,9 +11,7 @@ public class Bilhete {
     public Bilhete(Usuario usuario,Filme filme, Sessao sessao, Sala sala, Compra compra){
         this.nome = usuario.getUser();
         this.cpf = usuario.getCpf();
-        this.filme = filme.getNome();
-        this.sessao = sessao.getSessao(); //não sei os nomes dos atributos que vão ser na sessão, sala e compra
-        this.sala = sala.getSala();
+        this.sessao = sessao; //não sei os nomes dos atributos que vão ser na sessão, sala e compra
         this.valor = compra.getValor();
         this.cadeiraComprada = compra.getCadeira();
 
