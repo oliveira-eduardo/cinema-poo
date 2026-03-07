ca2fbf1cec57e287a61fe33e926ab3c07a131760
Eduardo Oliveira <oliveira.eduardo1104@gmail.com>
2026-03-07 18:47:23 -0300

    fix: recolocando cadastro no arquivo main

--- Main.java
+++ Main.java
@@ -3,8 +3,74 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        
-        Usuario usuario = Usuario.Cadastro();
+        Usuario usuario = Cadastro();
 
+    }
+
+    public static Usuario Cadastro(){
+        String user;
+        int idade;
+        String cpf;
+        String senha;
+        String sexo;
+        String email;
+        String numero_do_cartao;
+        String nome_do_cartao;
+        String codigo_verificador_do_cartao;
+
+        Scanner entrada = new Scanner(System.in);
+
+        do{
+            System.out.println("Digite seu nome de usuário:");
+            user = entrada.nextLine();
+
+        }while(!isOnlyLetter(user));
+
+        do { 
+            System.out.println("Digite seu os 11 digitos do seu CPF:");
+            cpf = entrada.nextLine();            
+        } while (!verificarCpf(cpf));
+
+        System.out.println("Digite sua senha:");
+        senha = entrada.nextLine();
+
+        do { 
+            System.out.println("Digite sua idade:");
+            idade = entrada.nextInt();            
+        } while (!verificadorIdade(idade));
+
+        entrada.nextLine(); // Limpar o buffer do scanner
+        System.out.println("Digite seu sexo:");
+        sexo = entrada.nextLine();
+
+        System.out.println("Digite seu email:");
+        email = entrada.nextLine();
+
+        System.out.println("Digite o nome do cartão:");
+        nome_do_cartao = entrada.nextLine();
+
+        System.out.println("Digite o número do cartão:");
+        numero_do_cartao = entrada.nextLine();
+
+        System.out.println("Digite o código verificador do cartão:");
+        codigo_verificador_do_cartao = entrada.nextLine();
+        
+        entrada.close();
+        Usuario usuario = new Usuario(user, cpf, senha, idade, sexo, email, nome_do_cartao, numero_do_cartao, codigo_verificador_do_cartao);
+        System.out.println(usuario);
+        return usuario;
+    }
+
+    public static boolean isOnlyLetter(String nome){ 
+        boolean r = nome.matches("[\\p{L}\\s]+");
+        return r;
+    }
+    public static boolean verificarCpf(String cpf){
+        boolean c = cpf.trim().matches("\\d{11}");
+        return c;
+    }
+    public static boolean verificadorIdade(int idade){
+        return idade >= 1 && idade <= 130;
     }
 
 }
--- Usuario.java
+++ Usuario.java
@@ -94,66 +94,6 @@ public class Usuario {
     public String getCodigo_verificador_do_cartao() {
         return codigo_verificador_do_cartao;
     }
-
-    public static Usuario Cadastro(){
-        String user;
-        String cpf;
-        int idade;
-        Scanner entrada = new Scanner(System.in);
-
-        do{
-            System.out.println("Digite seu nome de usuário:");
-            user = entrada.nextLine();
-
-        }while(!isOnlyLetter(user));
-        do { 
-            System.out.println("Digite seu os 11 digitos do seu CPF:");
-            cpf = entrada.nextLine();            
-        } while (!verificarCpf(cpf));
-
-        System.out.println("Digite sua senha:");
-        String senha = entrada.nextLine();
-
-        do { 
-            System.out.println("Digite sua idade:");
-            idade = entrada.nextInt();            
-        } while (!verificadorIdade(idade));
-
-
-        entrada.nextLine(); // Limpar o buffer do scanner
-        System.out.println("Digite seu sexo:");
-        String sexo = entrada.nextLine();
-
-        System.out.println("Digite seu email:");
-        String email = entrada.nextLine();
-
-        System.out.println("Digite o nome do cartão:");
-        String nome_do_cartao = entrada.nextLine();
-
-        System.out.println("Digite o número do cartão:");
-        String numero_do_cartao = entrada.nextLine();
-
-        System.out.println("Digite o código verificador do cartão:");
-        String codigo_verificador_do_cartao = entrada.nextLine();
-        
-        Usuario usuario = new Usuario(user, cpf, senha, idade, sexo, email, nome_do_cartao, numero_do_cartao, codigo_verificador_do_cartao);
-        System.out.println(usuario);
-        entrada.close();
-
-        return usuario;
-    }
-
-    public static boolean isOnlyLetter(String nome){ 
-        boolean r = nome.matches("[\\p{L}\\s]+");
-        return r;
-    }
-    public static boolean verificarCpf(String cpf){
-        boolean c = cpf.trim().matches("\\d{11}");
-        return c;
-    }
-    public static boolean verificadorIdade(int idade){
-        return idade >= 1 && idade <= 130;
-    }
  
     @Override
     public String toString(){
